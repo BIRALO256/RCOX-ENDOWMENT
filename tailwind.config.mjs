a142d51bc80faef3fd9bf3279a27e4415ae76501
MUWANGUZI JOVIC BIRALO <95884686+BIRALO256@users.noreply.github.com>
2024-02-25 14:23:17 +0300

    new changes

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,16 +3,20 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
+			padding:{
+				'1/3':'33.33333%',
+                '2/3':'66.6666%',
+			},
 			colors: {
 				
 				"jovic": "#5CB85C",
 				"jovic-white" :"#ffffff",
-				"jovic-yellow":"#fbaa00",
 				"xephas-white": "#ffffff",
 				"xephas-navy": "#000525",
 				"xephas-yellow": "#fbaa00",
 				"xephas-bg": "#ffffff",
-				"xephas-hover":"#1126bf"
+				"xephas-hover":"#1126bf",
+				"jovic-background":"#4A90E2"
 				
 			  },
 		},
