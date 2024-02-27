8841cb8b65c9655b5b46525be72336058bd497ad
MUWANGUZI JOVIC BIRALO <95884686+BIRALO256@users.noreply.github.com>
2024-02-27 23:17:14 +0300

    deleted github psges configration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,27 +2,11 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
 
-// const SERVER_PORT =300
-// LOCALHOST_URL ='https://localhost:${SERVER_PORT}'
-// //the url to access your blog after deploying it somewhere like(github pages ectc)
-// const LIVE_URL ="https://biralo256.github.io/";
-// //this is  the astro command your  npm scripts runs
-// const SCRIPT =process.env.npm_lifecycle_script || "";
-// const isBuild =SCRIPT.includes("astro build");
-// let  BASE_URL = LOCALHOST_URL;
-// //when you're building your site in local or CI , you could just set your URL Manually
-// const  base='https://github.com/BIRALO256/RCOX-ENDOWMENT'
-
-// if(isBuild){
-//   BASE_URL =LIVE_URL;
-// }
-
 // https://astro.build/config
 export default defineConfig({
   server:{
     port:30,
   },
-  site: 'https://biralo256.github.io/',
-  base: '/',
+ 
   integrations: [tailwind()]
 });
