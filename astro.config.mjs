75f95d02250f7efbc67b18407738758ecca75b7f
MUWANGUZI JOVIC BIRALO <95884686+BIRALO256@users.noreply.github.com>
2024-02-27 22:25:39 +0300

    still modifying

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,26 +2,27 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
 
-const SERVER_PORT =300
-LOCALHOST_URL ='https://localhost:${SERVER_PORT}'
-//the url to access your blog after deploying it somewhere like(github pages ectc)
-const LIVE_URL ="https://biralo256.github.io/";
-//this is  the astro command your  npm scripts runs
-const SCRIPT =process.env.npm_lifecycle_script || "";
-const isBuild =SCRIPT.includes("astro build");
-let  BASE_URL = LOCALHOST_URL;
-//when you're building your site in local or CI , you could just set your URL Manually
-const  base='https://github.com/BIRALO256/RCOX-ENDOWMENT'
+// const SERVER_PORT =300
+// LOCALHOST_URL ='https://localhost:${SERVER_PORT}'
+// //the url to access your blog after deploying it somewhere like(github pages ectc)
+// const LIVE_URL ="https://biralo256.github.io/";
+// //this is  the astro command your  npm scripts runs
+// const SCRIPT =process.env.npm_lifecycle_script || "";
+// const isBuild =SCRIPT.includes("astro build");
+// let  BASE_URL = LOCALHOST_URL;
+// //when you're building your site in local or CI , you could just set your URL Manually
+// const  base='https://github.com/BIRALO256/RCOX-ENDOWMENT'
 
-if(isBuild){
-  BASE_URL =LIVE_URL;
-}
+// if(isBuild){
+//   BASE_URL =LIVE_URL;
+// }
 
 // https://astro.build/config
 export default defineConfig({
   server:{
-    port:SERVER_PORT
+    port:300,
   },
-  site:BASE_URL,
+  site: 'https://astronaut.github.io',
+  base: '/RCOX-ENDOWMENT',
   integrations: [tailwind()]
 });
