2e8f7f9fb8eb791c9af4191b2f333f99f0fa60fd
MUWANGUZI JOVIC BIRALO <95884686+BIRALO256@users.noreply.github.com>
2024-02-28 10:40:49 +0300

    built the file with adtro build command and then added adaptor for vecel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
   server: {
     port: 30
   },
   integrations: [tailwind()],
- 
+  output: "server",
+  adapter: vercel()
 });
